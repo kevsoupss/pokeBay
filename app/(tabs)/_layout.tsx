61c40faba57cd13a714efbb79ae3f5349427531b
nathanter <nathan.yan.net@gmail.com>
2025-04-05 17:54:18 -0700

    ui

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,36 +1,79 @@
-import { View, Text } from 'react-native'
+import { View, Text, TextBase, ImageBackground } from 'react-native'
 import React from 'react'
 import { Tabs } from 'expo-router'
+import { Image } from 'react-native'
+import { icons } from '@/constants/icons'
+import { images } from '@/constants/images'
 
+const Tab_icon = ({focused,icon,title}: any) => {
+   
+    
+    if(focused){
+        return(
+            <>
+            <ImageBackground source={images.buttonback} tintColor="#" className='flex flex-row flex-1 min-w-[75px] justify-center items-center gap-1'>
+            <Image source = {icon} className = "size-4" tintColor="#151312"/>
+            <Text>{title}</Text>
+            </ImageBackground>
+            </>
+            
+        )
+    }
+    else{
+        return(<Image source = {icon} className = "size-5"/> )
+    }
+
+    
+    }
 const _Layout = () => {
   return (
-    <Tabs>
+    <Tabs
+        screenOptions = {{
+            tabBarShowLabel : false,
+            tabBarStyle : {
+                backgroundColor: '#231D58'
+
+            }
+        }}
+    >
         <Tabs.Screen 
             name="index"
             options={{
                 title: 'Home',
-                headerShown:false
+                headerShown:false,
+                tabBarIcon : ({focused}:any) => (
+                   <Tab_icon focused = {focused} icon = {icons.home} title = 'Home' />
+                ) 
             }}
         />
         <Tabs.Screen 
             name="camera"
             options={{
                 title: 'Camera',
-                headerShown:false
+                headerShown:false,
+                tabBarIcon : ({focused} : any) => (
+                    <Tab_icon focused = {focused} icon = {icons.camera} title = 'Camera' />
+                 ) 
             }}
         />
         <Tabs.Screen 
             name="cards"
             options={{
                 title: 'Cards',
-                headerShown:false
+                headerShown:false,
+                tabBarIcon : ({focused} : any) => (
+                    <Tab_icon focused = {focused} icon = {icons.card} title = 'Cards' />
+                 ) 
             }}
         />
         <Tabs.Screen 
             name="shop"
             options={{
                 title: 'Shop',
-                headerShown:false
+                headerShown:false,
+                tabBarIcon : ({focused} : any) => (
+                    <Tab_icon focused = {focused} icon = {icons.shop} title = 'Shop'/>
+                 ) 
             }}
         />
     </Tabs>
--- constants/images.ts
+++ constants/images.ts
@@ -1,7 +1,9 @@
 import ebaylogo from "@/assets/images/ebaylogo.png";
 import logo from "@/assets/images/logo.png";
+import buttonback from "@/assets/images/buttonback.png"
 
 export const images = {
     ebaylogo,
-    logo
+    logo,
+    buttonback,
 };
