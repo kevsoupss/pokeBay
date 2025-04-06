0ee0daf505e2f56c07824af983b7c752ad775aa8
Kevin Lei <kevinleimc@gmail.com>
2025-04-05 18:36:31 -0700

    edit app.json

---
+++ app/_layout.tsx
@@ -0,0 +1,6 @@
+import { Slot } from "expo-router";
+import {AuthProvider} from '../context/AuthContext'
+
+export default function RootLayout() {
+  return <AuthProvider><Slot /></AuthProvider>;
+}
